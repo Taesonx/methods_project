b1873bc98fde6650d60d924ec5074c15bfa00d2c
Аня <annalyashko160107@gmail.com>
2026-04-24 13:14:08 +0300

    Update Function29.h

--- Function29.h
+++ Function29.h
@@ -1,10 +1,13 @@
-#pragma once
-// Функция №29 с параметрами
-// Область определения: x >= 0
-// Параметры: a, b, c, d, e
+#ifndef FUNCTION29_H
+#define FUNCTION29_H
+
+#include <cmath>
+
 double f29(double x, double a, double b, double c, double d, double e) {
-    if (x < 0) return 0;
     if (x >= 0 && x < 1) return a * sqrt(x);
-    if (x >= 1 && x < 2) return fabs(b / x);
-    return fabs(c * x * x - d * x + e);
+    if (x >= 1 && x < 2) return b / x;
+    if (x >= 2) return c * x * x - d * x + e;
+    return 0; // РґР»СЏ x < 0
 }
+
+#endif
